4e69a1ccc03bcd3834715ebf3adeba047af41859
kumarnihar67 <51451838+kumarnihar67@users.noreply.github.com>
2022-02-19 10:12:57 +0530

    check copy array

---
+++ myjavaProject/31dec/copyarray.java
@@ -0,0 +1,14 @@
+class ArrayCopy 
+{
+public static void main(string[]args)
+{
+      char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e',  
+                'i', 'n', 'a', 't', 'e', 'd' };
+
+        char[] copyTo = new char[7];  
+      
+        System.arraycopy(copyFrom, 2, copyTo, 0, 7);  
+          
+        System.out.println(String.valueOf(copyTo));  
+    }  
+}  
