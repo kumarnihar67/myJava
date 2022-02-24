715404df59a0a7b666a75b53f46aed95d062525e
kumarnihar67 <51451838+kumarnihar67@users.noreply.github.com>
2022-02-24 10:38:50 +0530

    check matrix multiplication

---
+++ myjavaProject/31dec/matrixmultiplication.java
@@ -0,0 +1,21 @@
+public class MatrixMultiplicationExample{
+public static void main(String args[])
+{
+int a[][]={{1,2,3},{2,3,4},{3,4,5}};  
+int b[][]={{3,2,1},{4,3,2},{5,4,3}};    
+int c[][]=new int[3][3];    
+for(int i=0;i<3;i++)
+{  
+for(int j=0;j<3;j++)
+{  
+c[i][j]=0;    
+for(int k=0;k<3;k++)    
+{    
+c[i][j]+=a[i][k]*b[k][j];    
+}
+System.out.print(c[i][j]+" "); 
+}
+System.out.println()
+}  
+}
+}
